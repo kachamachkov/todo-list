2b49644d12a9f6e883757eb9b8e14e825f2a15ce
kachamachkov <kachamachkovboyan@gmail.com>
2024-09-15 18:46:52 +0300

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Header from './components/Header';
+import Home from './components/Home';
+import Pricing from './components/Pricing';
 import TodoList from './components/TodoList';
+
 function App() {
   return (
     <>
       <Header />
-      <TodoList />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/pricing' element={<Pricing />} />
+      </Routes>
     </>
   );
 }
