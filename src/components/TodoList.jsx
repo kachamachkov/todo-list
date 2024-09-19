c97548d0414ac3dd98f0459f6bbd10197f26c1c5
kachamachkov <kachamachkovboyan@gmail.com>
2024-09-19 19:08:07 +0300

    Load todos with react-query
    Remove state/useEffect

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
@@ -12,7 +13,11 @@ import IconButton from '@mui/material/IconButton';
 import CommentIcon from '@mui/icons-material/Comment';
 
 export default function TodoList() {
-  const [todos, setTodos] = useGetTodos();
+  const todosQuery = useQuery({
+    queryKey: ['todos'],
+    queryFn: getTodos,
+  });
+
   const [checked, setChecked] = React.useState([0]);
 
   const handleToggle = (value) => () => {
@@ -28,60 +33,59 @@ export default function TodoList() {
     setChecked(newChecked);
   };
 
+  console.log(todosQuery.data);
+
   return (
     <Container component='main' maxWidth='xs'>
       <CssBaseline />
       <h1>Todo List</h1>
-      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-        {todos.map((todo) => {
-          const labelId = `checkbox-list-label-${todo.id}`;
+      {todosQuery.isFetching ? (
+        <div>Loading Todos</div>
+      ) : (
+        <List
+          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        >
+          {todosQuery.data.map((todo) => {
+            const labelId = `checkbox-list-label-${todo.id}`;
 
-          return (
-            <ListItem
-              key={todo.id}
-              secondaryAction={
-                <IconButton edge='end' aria-label='comments'>
-                  <CommentIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton
-                role={undefined}
-                onClick={handleToggle(todo.id)}
-                dense
+            return (
+              <ListItem
+                key={todo.id}
+                secondaryAction={
+                  <IconButton edge='end' aria-label='comments'>
+                    <CommentIcon />
+                  </IconButton>
+                }
+                disablePadding
               >
-                <ListItemIcon>
-                  <Checkbox
-                    edge='start'
-                    checked={checked.indexOf(todo.id) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': labelId }}
-                  />
-                </ListItemIcon>
-                <ListItemText id={labelId} primary={todo.todo} />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
+                <ListItemButton
+                  role={undefined}
+                  onClick={handleToggle(todo.id)}
+                  dense
+                >
+                  <ListItemIcon>
+                    <Checkbox
+                      edge='start'
+                      checked={checked.indexOf(todo.id) !== -1}
+                      tabIndex={-1}
+                      disableRipple
+                      inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                  </ListItemIcon>
+                  <ListItemText id={labelId} primary={todo.todo} />
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
     </Container>
   );
 }
 
-function useGetTodos() {
-  const [todos, setTodos] = React.useState([]);
+async function getTodos() {
+  const response = await fetch('https://dummyjson.com/todos');
+  const result = await response.json();
 
-  React.useEffect(() => {
-    (async () => {
-      const response = await fetch('https://dummyjson.com/todos');
-
-      const result = await response.json();
-
-      setTodos(result.todos);
-    })();
-  }, []);
-
-  return [todos, setTodos];
+  return result.todos;
 }
